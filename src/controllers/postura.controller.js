8972deca30766e4daafa7cf69dbbf6c193645e4d
hernancaceres <harinatha777@hotmail.com>
2024-12-15 22:57:27 -0300

    api basica completa antes de agregar blog

--- src/controllers/postura.controller.js
+++ src/controllers/postura.controller.js
@@ -54,54 +54,6 @@ export const crearPostura = async (req, res) => {
 };
 
 
-
-// // Crear una nueva postura
-// export const crearPostura = async (req, res) => {
-//   const { nombre, descripcion, duracion, instrucciones } = req.body;
-//   const { claseId } = req.params; // ID de la clase, si está disponible en la ruta
-
-//   try {
-//     console.log('Datos recibidos para crear postura:', { nombre, descripcion, duracion, instrucciones, claseId });
-
-//     // Verificar si la postura debe asociarse con una clase específica
-//     if (claseId) {
-//       // Validar que la clase exista
-//       const clase = await Clase.findByPk(claseId);
-//       if (!clase) {
-//         return res.status(404).json({ error: 'Clase no encontrada' });
-//       }
-
-//       // Crear la postura y asociarla a la clase
-//       const nuevaPostura = await Postura.create({ nombre, descripcion, duracion, instrucciones, claseId: clase.id });
-
-//       console.log('Postura creada y asociada a la clase:', nuevaPostura);
-//       return res.status(201).json({
-//         message: 'Postura creada y asociada a la clase exitosamente',
-//         postura: nuevaPostura,
-//       });
-//     } else {
-//       // Crear una postura sin clase específica
-//       const nuevaPostura = await Postura.create({ nombre, descripcion, duracion, instrucciones });
-//       console.log('Postura creada exitosamente sin clase:', nuevaPostura);
-//       return res.status(201).json(nuevaPostura);
-//     }
-//   } catch (error) {
-//     console.error('Error al crear la postura:', error);
-//     res.status(500).json({ error: 'Error al crear la postura', detalles: error.message });
-//   }
-// };
-
-// // Crear una nueva postura
-// export const crearPostura = async (req, res) => {
-//   const { nombre, descripcion, duracion, claseId } = req.body;
-//   try {
-//     const nuevaPostura = await Postura.create({ nombre, descripcion, duracion, claseId });
-//     res.json(nuevaPostura);
-//   } catch (error) {
-//     res.status(500).json({ error: 'Error al crear la postura' });
-//   }
-// };
-
 // Obtener todas las posturas
 export const obtenerPosturas = async (req, res) => {
   try {
@@ -129,7 +81,7 @@ export const obtenerPosturaPorId = async (req, res) => {
 // Actualizar una postura
 export const actualizarPostura = async (req, res) => {
   const { id } = req.params;
-  const { nombre, descripcion, duracion } = req.body;
+  const { nombre, descripcion, duracion, instrucciones, imagenUrl } = req.body;
   try {
     const postura = await Postura.findByPk(id);
     if (!postura) {
@@ -139,6 +91,7 @@ export const actualizarPostura = async (req, res) => {
     postura.descripcion = descripcion;
     postura.instrucciones = instrucciones;
     postura.duracion = duracion;
+    postura.imagenUrl = imagenUrl;
     await postura.save();
     res.json(postura);
   } catch (error) {
--- src/models/Postura.js
+++ src/models/Postura.js
@@ -37,32 +37,3 @@ const Postura = sequelize.define(
 
 export default Postura;
 
-
-
-
-// import { DataTypes } from 'sequelize';
-// import sequelize from '../database/database.js';
-
-// const Postura = sequelize.define('Postura', {
-//   nombre: {
-//     type: DataTypes.STRING,
-//     allowNull: false,
-//   },
-//   descripcion: {
-//     type: DataTypes.TEXT,
-//     allowNull: false,
-//   },
-//   duracion: {
-//     type: DataTypes.INTEGER, // tiempo que se debe sostener la postura
-//     allowNull: false,
-//   },
-//   instrucciones: {
-//     type: DataTypes.TEXT,
-//     allowNull: false,
-//   },
-// }, {
-//   timestamps: true,
-// });
-
-// export default Postura;
-
